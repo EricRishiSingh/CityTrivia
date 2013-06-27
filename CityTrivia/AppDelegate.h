d0e89f065f6cc5bcb7c7d7002f48f32db2c6c919
Eric Singh <ericsingh@Erics-Mac-mini.local>
2013-06-26 22:44:56 -0400

    Initial Commit

---
+++ CityTrivia/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CityTrivia
+//
+//  Created by Eric Singh on 2013-06-26.
+//  Copyright (c) 2013 Eric Singh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CityTrivia/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CityTrivia
+//
+//  Created by Eric Singh on 2013-06-26.
+//  Copyright (c) 2013 Eric Singh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
